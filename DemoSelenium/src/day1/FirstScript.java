d93d080ed6c0c84a1348c9a0e2abae67da101ce6
mprem <mprem@DESKTOP-POVA60A>
2020-05-11 16:07:02 +0530

    Demom

---
+++ DemoSelenium/src/day1/FirstScript.java
@@ -0,0 +1,27 @@
+package day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstScript {
+
+	public static void main(String[] args) {
+
+		// WebDriver is Interface & FirefoxDriver , ChromeDriver, OperaDriver
+		// are Classes which implements WebDriver
+		// WebDriver is Parent & ChromeDriver | OperaDriver | FirefoxDriver
+		// are Child
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mprem\\OneDrive\\Desktop\\SelJar\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		// Parent p=new Child > Only Parent Specific functions will be
+		// visible & rest every functions will be hidden
+
+		driver.get("http://selenium.dev"); // Opening URL
+
+		System.out.println(driver.getTitle());  // Pronting Title
+
+		driver.quit(); // Closing Browser
+
+	}
+
+}
